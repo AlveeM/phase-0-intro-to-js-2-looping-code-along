25b99c91373ab6d34b2213ad99b449174801c52f
Alvee Akand <alvee.akand@outlook.com>
2021-03-16 16:05:28 -0400

    done

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Code your solutions in this file
+
+function writeCards(names, occasion) {
+  const res = [];
+  for (let name of names) {
+    res.push(`Thank you, ${name}, for the wonderful ${occasion} gift!`);
+  }
+  return res;
+}
+
+function countDown(num) {
+  for (let i = num; i >= 0; i--) {
+    console.log(i);
+  }
+}
